c9f53574fcd09f2d7ec272c0c9b526e007d92fef
Romeo <spacework@Romeos-iMac.local>
2023-06-01 00:03:39 -0600

    Cambio Mayo 31 Casa (extra)

--- src/context/Cart.jsx
+++ src/context/Cart.jsx
@@ -8,7 +8,25 @@ export const CartProvider = ( { children } ) => {
 
     const [ cart, setCart ] = useState([])
 
-    const addToCart = product => {}
+    const addToCart = product => {
+
+        // Check if the product is already in the cart 
+        const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+        // Si el producto ya esta en el carrito 
+        if(productInCartIndex >= 0) {
+            const newCart = structuredClone(cart)
+            newCart[productInCartIndex].quantity += 1
+            return setCart(newCart)
+        }
+
+        // Si el producto no esta en el carrito 
+        setCart(prevState => ([
+            ...prevState,
+            { ...product, quantity: 1 }
+        ]))
+
+    }
     const clearCart = () => { setCart([]) }
 
     return (
