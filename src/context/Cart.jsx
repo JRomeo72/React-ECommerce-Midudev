58a041703b5a8570de67a1f9cccc2b1d6840f811
Romeo Hernandez <romeo.hernandez@Mac-mini-de-Estudio.local>
2023-06-03 14:03:20 -0600

    Cambios Junio 3 2023 Bla

--- src/context/Cart.jsx
+++ src/context/Cart.jsx
@@ -1,12 +1,71 @@
-import { createContext, useState } from "react"
+import { createContext, useReducer, useState } from "react"
 
 
 export const CartContext = createContext()
 
+// > Usando Reducer
+const initialState = []
+const reducer = ( state, action ) => {
+
+    const { type: actionType, action: actionPayload } = action
+    
+    switch (actionType) {
+
+        case 'ADD_TO_CART':
+            {
+                const { id } = actionPayload
+                // Check if the product is already in the cart 
+                const productInCartIndex = state.findIndex(item => item.id === id)
+    
+                // Si el producto ya esta en el carrito 
+                if(productInCartIndex >= 0) {
+                    const newCart = structuredClone(state)
+                    newCart[productInCartIndex].quantity += 1
+                    return newCart
+                }
+    
+                // Si el producto no esta en el carrito 
+                return [
+                    ...state,
+                    { ...actionPayload, quantity: 1 }
+                ]
+            }
+
+        case 'REMOVE_FROM_CART':
+            {
+                const { id } = actionPayload
+                return state.filter(item => item.id !== id)
+            }
+
+        case 'CLEAR_CART':
+            {
+                return initialState
+            }
+    
+        default:
+            break;
+    }
+
+}
+
 // eslint-disable-next-line react/prop-types
 export const CartProvider = ( { children } ) => {
 
-    const [ cart, setCart ] = useState([])
+    const [ state, dispatch ] = useReducer(reducer, initialState)
+
+    const addToCart = product => dispatch({
+        type: 'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type: 'REMOVE_FROM_CART'
+    })
+
+
+
+    // > Sin Reducer 
+    /*const [ cart, setCart ] = useState([])
 
     const addToCart = product => {
 
@@ -32,10 +91,10 @@ export const CartProvider = ( { children } ) => {
         setCart(prevState => prevState.filter(item => item.id !== product.id))
     }
     
-    const clearCart = () => { setCart([]) }
+    const clearCart = () => { setCart([]) } */
 
     return (
-        <CartContext.Provider value={ { cart, addToCart, removeFromCart, clearCart } }>
+        <CartContext.Provider value={ { cart: state, addToCart, removeFromCart, clearCart } }>
             { children }
         </CartContext.Provider>
     )
